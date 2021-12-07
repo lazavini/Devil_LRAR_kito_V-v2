e5e75e5f114e672efbd938f3fc2bbc20fe0de619
Charles Leal Coragem <charleslealcoragem@hotmail.com>
2021-12-07 20:01:23 -0300

    Correção no color Card

--- Assets/Vuforia/Scripts/Cards/ColorCard.cs
+++ Assets/Vuforia/Scripts/Cards/ColorCard.cs
@@ -11,7 +11,8 @@ namespace Assets.Vuforia.Scripts.Cards
 {
     public class ColorCard : ModificationCard
     {
-        FlexibleColorPicker ColorPicker => Status == TrackableBehaviour.Status.TRACKED ? CardPanel.gameObject.GetComponentInChildren<FlexibleColorPicker>(true) : null;
+        private ICollection<string> Colors;
+        Dropdown ColorPicker => Status == TrackableBehaviour.Status.TRACKED ? CardPanel.gameObject.GetComponentInChildren<Dropdown>(true) : null;
         public string SelectedColor { get; set; }
         public ColorCard(string name,
             string sound, 
@@ -22,6 +23,18 @@ namespace Assets.Vuforia.Scripts.Cards
             animation,
             CardType.Color)
         {
+            Colors = new List<string>
+            {
+                "Branca", //#F8F8FF
+                "Azul", //#0000FF
+                "Rosa", //#FF1493
+                "Vermelha",//#FF0000
+                "Verde" , //#008000
+                "Preta", //#000000
+                "Amarela" , //#FFFF00
+                "Roxa",//#8A2BE2
+                "Laranja" //#FF4500
+            };
         }
         public override void CardTrackChanged(TrackableBehaviour.Status status)
         {
@@ -31,16 +44,10 @@ namespace Assets.Vuforia.Scripts.Cards
         public override void Mix(ICard card)
         {
             Color color;
-            if(SelectedColor != null)
-            {
+            if (SelectedColor != null)
                 ColorUtility.TryParseHtmlString(SelectedColor, out color);
-            }
             else
-                color = ColorPicker?.color ??
-                    new Color(UnityEngine.Random.Range(0, 1f),
-                    UnityEngine.Random.Range(0, 1f),
-                    UnityEngine.Random.Range(0, 1f),
-                    1);
+                ColorUtility.TryParseHtmlString(ConvertColor(Colors.ElementAt(ColorPicker?.value ?? (new System.Random()).Next(0, Colors.Count - 1))), out color);
 
             if (CardComponent == null)
             {
@@ -56,6 +63,34 @@ namespace Assets.Vuforia.Scripts.Cards
                     render.material.SetColor("_Color", color);
             }
             SelectedColor = ColorUtility.ToHtmlStringRGBA(color);
+        }
+
+        private static string ConvertColor(string color)
+        {
+            switch (color)
+            {
+                case "Branca":
+                    return "#F8F8FF";
+                case "Azul":
+                    return "#0000FF";
+                case "Rosa":
+                    return "#FF1493";
+                case "Vermelha":
+                    return "#FF0000";
+                case "Verde":
+                    return "#008000";
+                case "Preta":
+                    return "#000000";
+                case "Amarela":
+                    return "#FFFF00";
+                case "Roxa":
+                    return "#8A2BE2";
+                case "Laranja":
+                    return "#FF4500";
+                default:
+                    return "";
+            }
+
         }
     }
 }
